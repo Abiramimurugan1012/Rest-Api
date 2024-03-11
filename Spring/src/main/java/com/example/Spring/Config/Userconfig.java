e5a3a90503b97bf8a217079eefcee08c82ed3508
abiramimurugan1012 <muruganabirami476@gmail.com>
2024-03-11 15:44:30 +0530

    empty

--- Spring/src/main/java/com/example/Spring/Config/Userconfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.Spring.Config;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-
-public class Userconfig {
-}
