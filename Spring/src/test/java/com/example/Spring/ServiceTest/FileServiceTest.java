8283c4388a1f14e433c9a5901ac0276ef1bf5af8
MDAshik786 <rioashik786@gmail.com>
2024-03-11 14:39:08 +0530

    newupdate

--- Spring/src/test/java/com/example/Spring/ServiceTest/FileServiceTest.java
+++ Spring/src/test/java/com/example/Spring/ServiceTest/FileServiceTest.java
@@ -15,8 +15,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
@@ -68,13 +67,6 @@ public class FileServiceTest {
             fileService.storeFile(multipartFile);
         });
     }
-
-
-
-
-
-
-
 
 
 }
